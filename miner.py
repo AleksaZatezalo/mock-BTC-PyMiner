6224b893860b69cc3e7436b0a58f5c51558eb862
Aleksa <zabumaphu@gmail.com>
2021-02-02 18:55:39 -0500

    Hash function created.

--- miner.py
+++ miner.py
@@ -6,4 +6,10 @@ get an intuitive understanding of BTC mining.
 """
 
 from hashlib import sha256
+
+print("Example hash by SHA256 of 'ABC'")
 print(sha256("ABC".encode("ascii")).hexdigest())
+
+def SHA256(text):
+    return sha256(text.encode("ascii")).hexdigest()
+
